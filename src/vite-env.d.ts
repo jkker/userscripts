cb7720eb5213568cb89da1365ae8b12c139c4bcb
jkker <j@erry.dev>
2026-05-17 19:37:29 -0700

    chore: add hosted git info types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-monkey/global" />
-
-declare module 'hosted-git-info' {
-  export type HostedGitInfo = {
-    file(path: string): string
-    bugs(): string
-  }
-
-  const hostedGitInfo: {
-    fromUrl(url: string): HostedGitInfo | undefined
-  }
-
-  export default hostedGitInfo
-}
